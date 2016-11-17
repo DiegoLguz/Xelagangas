6a22523925dea9b22c928ec3261887838f9cea4e
Diego <diegolguz14@gmail.com>
2016-11-17 05:18:27 +0100

    permisos para agregar Artista a usuarios

--- posts/views.py
+++ posts/views.py
@@ -48,10 +48,6 @@ def signup(request):
     return render(request,"signup.html",data)
 
 def entrada(request):
-    if not request.user.is_staff or not request.user.is_superuser:
-        raise Http404
-    if not request.user.is_staff or not request.user.is_superuser:
-        raise Http404
     form = ArchivoForm(request.POST or None,request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
